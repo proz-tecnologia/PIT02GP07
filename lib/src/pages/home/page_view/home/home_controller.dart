c2ef09f29486276d61e27197c097078cddc2d7d5
Paula F Rodrigues <pfr.blackcat@gmail.com>
2022-12-02 22:55:26 -0300

    transactions screen created

--- lib/src/pages/home/page_view/home/home_controller.dart
+++ lib/src/pages/home/page_view/home/home_controller.dart
@@ -1,13 +1,14 @@
 import 'package:pit02gp07/src/model/expenses_model.dart';
 
 class HomeController {
-  
   final typeList = <String>["Despesa", "Receita"];
   final categoriesList = <String>["Alimentação", "Educação"];
   final accountTypeList = <String>["Carteira", "Cartão"];
 
   final expensesList = <ExpenseModel>[];
+  int get listExpensesLength => expensesList.length;
   
+
   void addExpenseList(ExpenseModel newValue) {
     expensesList.add(newValue);
   }
--- lib/src/pages/transaction/transactions_screen.dart
+++ lib/src/pages/transaction/transactions_screen.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:pit02gp07/src/core/theme/app_colors.dart';
-import 'package:pit02gp07/src/core/theme/app_text_style.dart';
-import 'package:pit02gp07/src/pages/home/cards/components/expenses_card_widget.dart';
 
-import 'package:pit02gp07/src/pages/home/cards/components/wallet_type_widget.dart';
+import '../../model/expenses_model.dart';
+import 'transaction_card.dart';
+import 'transaction_resume_card.dart';
 
-class TransactionsScreen extends StatelessWidget {
+class TransactionsScreen extends StatefulWidget {
+   final String revenueValue;
+  final String expenseValue;
+
+   final int listExpensesLength;
+  final List<ExpenseModel> listExpenses;
+
   const TransactionsScreen({
-    super.key,
-  });
+    Key? key,
+    required this.revenueValue,
+    required this.expenseValue,
+    required this.listExpensesLength,
+    required this.listExpenses,
+  }) : super(key: key);
 
   static const BottomNavigationBarItem item = BottomNavigationBarItem(
     icon: Icon(Icons.compare_arrows_outlined),
@@ -17,138 +26,37 @@ class TransactionsScreen extends StatelessWidget {
   );
 
   @override
+  State<TransactionsScreen> createState() => _TransactionsScreenState();
+}
+
+class _TransactionsScreenState extends State<TransactionsScreen> {
+  @override
   Widget build(BuildContext context) {
-    return ListView(
-      padding: const EdgeInsets.only(right: 10.0, left: 10.0),
-      // ignore: prefer_const_literals_to_create_immutables
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Card(
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 24.0, horizontal: 8.0),
+      child: Column(
+        children: [
+          Card(
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(16.0),
             ),
             elevation: 8.0,
             child: Padding(
               padding: const EdgeInsets.all(28.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: const [
-                  WalletTypeWidget(
-                      icon: Icons.arrow_circle_up_outlined,
-                      colorIcon: AppColors.iceWhite,
-                      backgroundColor: AppColors.mediumGreen,
-                      value: '0,00',
-                      type: 'Receitas',
-                      style: AppTextStyle.mediumWhite),
-                  SizedBox(
-                    height: 12.0,
-                  ),
-                  WalletTypeWidget(
-                      icon: Icons.arrow_circle_up_outlined,
-                      colorIcon: AppColors.iceWhite,
-                      backgroundColor: AppColors.lightRed,
-                      value: '0,00',
-                      type: 'Despesas',
-                      style: AppTextStyle.mediumWhite)
-                ],
-              ),
+              child:  TransactionResumeCard(
+                revenueValue: widget.revenueValue,
+                expenseValue: widget.expenseValue,
+                ),
             ),
           ),
-        ),
-        const Padding(
-          padding: EdgeInsets.only(left: 16.0),
-          child: Text(
-            'Data',
-            style: TextStyle(
-                fontWeight: FontWeight.bold, color: AppColors.iceWhite),
+          ListView.builder(
+            itemCount: 1,
+            itemBuilder: (BuildContext context, int index) {
+              return const TransactionCard(category: '', value: '',);
+            },
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(16.0),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Column(
-                children: const [
-                  ExpensesCardWidget(
-                    category: 'Nome da despesa 1',
-                    value: '0,11',
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(16.0),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Column(
-                children: const [
-                  ExpensesCardWidget(
-                    category: 'Nome da despesa 2',
-                    value: '0,12',
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
-        const Padding(
-          padding: EdgeInsets.only(left: 16.0),
-          child: Text(
-            'Data',
-            style: TextStyle(
-                fontWeight: FontWeight.bold, color: AppColors.iceWhite),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(16.0),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Column(
-                children: const [
-                  ExpensesCardWidget(
-                    category: 'Nome da despesa 3',
-                    value: '0,13',
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(16.0),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Column(
-                children: const [
-                  ExpensesCardWidget(
-                    category: 'Nome da despesa 4',
-                    value: '0,14',
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
