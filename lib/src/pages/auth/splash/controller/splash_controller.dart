c639e3ab65583fad85a96b426fd8bb3aae84b6f5
MizzaelCosta <mizzaelcosta+github@gmail.com>
2022-12-20 00:04:12 -0300

    remove initializeFirebase

--- lib/src/pages/auth/splash/controller/splash_controller.dart
+++ lib/src/pages/auth/splash/controller/splash_controller.dart
@@ -1,7 +1,5 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pit02gp07/src/pages/auth/splash/state/splash_state.dart';
-
 import '../../repository/repository.dart';
 
 class SplashBloc extends Cubit<SplashState> {
@@ -12,21 +10,8 @@ class SplashBloc extends Cubit<SplashState> {
     }) : super(SplashStateLoading());
 
   Future<void> setup() async {
-    await initializeFirebase();
+    await repository.init();
     hasUser();
-  }
-
-  Future<FirebaseApp> initializeFirebase() async {
-    return await Firebase.initializeApp(
-      name: 'AcCount Finance App',
-      options: const FirebaseOptions(
-        apiKey: "AIzaSyA65apA03G1yrnZT1HjJmtGSdSz2Se8yDw",
-        appId: '1:494955119206:android:f8ed79a4fbd0b6c37563a7',
-        messagingSenderId:
-              '494955119206-ooais6ks5or3utqhj889s4h8sh689kbs.apps.googleusercontent.com',
-        projectId: 'account-finance-app',
-      ),
-    );
   }
 
   void hasUser() {
