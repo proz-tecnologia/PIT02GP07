75eb03f94c37957d39467020d5130139456a0e7a
Paula F Rodrigues <pfr.blackcat@gmail.com>
2023-01-22 14:43:31 -0300

    tests

--- test/src/pages/auth/login/login_mock.dart
+++ test/src/pages/auth/login/login_mock.dart
@@ -1,13 +1,9 @@
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:mocktail/mocktail.dart';
-import 'package:pit02gp07/src/pages/auth/repository/repository.dart';
+import 'package:pit02gp07/src/pages/auth/repository/repository_impl.dart';
 
-class MockAuthRepository extends Mock implements AuthRepository {}
+class MockAuthRepository extends Mock implements AuthRepositoryImpl {}
 
-class MockUser extends Mock implements User {}
+class UserMock extends Mock implements User {}
 
-class MockUserCredential extends Mock implements UserCredential {
-  @override
-  User get user => MockUser();
-}
+class MockUserCredential extends Mock implements UserCredential {}
